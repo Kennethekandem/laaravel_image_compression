85bb1b1b20228c67c0af1e317a549df6b1b6ecd5
Kenneth Ekandem <kennyekandem@gmail.com>
2023-04-16 13:47:18 +0100

    update: completed compression

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -3,21 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
+use Illuminate\Support\Facades\Validator;
 
-class ImageController extends Controller {
-    
-    public function compressImage(Request $request) {
-        
-        $this->validate($request, [
+class ImageController extends Controller
+{
+
+    public function compressImage(Request $request)
+    {
+
+        /* $this->validate($request, [
             'image' => 'required|images|mimes:jpg,jpeg,png,gif,svg|max:2048'
-        ]);
+        ]); */
 
-        $image = $request->file['image'];
+        $image = $request->file('image');
         /* 
             Note: Use $image = base64_decode($request['image'])
             if the image is sent as a base64 encoded  image.
          */
-        $image_name = time().'.'.$image->getClientOriginalExtension();
-        $path = public_path('/images');
+        $image_name = time() . '_' . $image->getClientOriginalName();
+        $path = public_path('uploads/') . "/" . $image_name;
+
+        Image::make($image->getRealPath())->resize(150, 150)->save($path);
+
+        return response()->json([
+            'data' => 'Image compressed and added'
+        ], 201);
     }
 }
