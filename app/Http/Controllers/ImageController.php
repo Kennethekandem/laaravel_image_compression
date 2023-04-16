bdf6eccbd8f8624473ec289eccaea21c5494cf17
Kenneth Ekandem <kennyekandem@gmail.com>
2023-04-16 10:35:58 +0100

    working on image compression controller

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -4,7 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class ImageController extends Controller
-{
+class ImageController extends Controller {
     
+    public function compressImage(Request $request) {
+        
+        $this->validate($request, [
+            'image' => 'required|images|mimes:jpg,jpeg,png,gif,svg|max:2048'
+        ]);
+
+        $image = $request->file['image'];
+        $image_name = time().'.'.$image->getClientOriginalExtension();
+    }
 }
